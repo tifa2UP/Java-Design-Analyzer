a904ddc73701665c86e8fb697c99735a46f06457
tifa2UP <tifa3333@gmail.com>
2016-04-25 17:26:31 -0700

    Added the definition of responsibility and instability

--- Main.java
+++ Main.java
@@ -10,17 +10,19 @@ import java.io.FilenameFilter;
 import java.lang.Class;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.lang.reflect.Type;
 import java.util.HashSet;
 
 
 public class Main {
 
     Class[] loadedPackage = null;
+    String[] classNames= null;
 
     public static void main(String[] args) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
-        System.out.println("test");
+//        System.out.println("test");
         Main main = new Main();
-        main.loadPackage("C:\\Users\\Abdellatif\\Desktop\\151\\Java-Design-Analyzer\\demo");
+        main.loadPackage("Demo");
     }
 
     private double inDepth(Class c) {
@@ -28,15 +30,41 @@ public class Main {
             c = c.getSuperclass();
             return 1 + inDepth(c);
         }
-        return 0;
+        return 1;
     }
 
+    /**
+     * Given the definition that
+
+     responsibility(A) = #clients(A)/#p
+
+     where A is any class, clients of A are other classes that use A and #p is the total number of classes
+
+     */
     private double responsibility(Class c) {
-        return 0;
+        int clientsNumber = 0;
+        for (Method m: c.getMethods()){
+           Type[] mTypes = m.getGenericParameterTypes();
+            for (Type t: mTypes){
+               for (int i = 0; i < classNames.length; i++){
+                   if (t.getTypeName() == classNames[i]){
+                       clientsNumber++;
+                   }
+               }
+            }
+        }
+        return clientsNumber/classNames.length;
     }
 
+    /**
+     *Given the definition that
+     instability(A) = #providers(A)/#p
+     where A is any class and providers of A are the classes that A depend on
+     (either through inheritance or composition). and #p is the total number of classes.
+     */
     private double instability(Class c) {
-        return 0;
+       int providersNumber = 0;
+        return providersNumber;
     }
 
     private double workload(Class c) {
